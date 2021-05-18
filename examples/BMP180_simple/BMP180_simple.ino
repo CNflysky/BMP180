11a0823eb94a7d099f32762fca19eda63f3e5da3
CNflysky <CNflysky@qq.com>
2021-05-18 12:41:07 +0800

    add info

--- examples/BMP180_simple/BMP180_simple.ino
+++ examples/BMP180_simple/BMP180_simple.ino
@@ -1,17 +1,19 @@
 #include <BMP180.h>
 BMP180 bmp180(0x77);
-void setup(){
-    Serial.begin(9600);
-    Serial.println();
-    bmp180.begin(BMP180_MODE_ULTRAHIRES);
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  bmp180.begin(BMP180_MODE_ULTRAHIRES);
 }
 
-void loop(){
-    Serial.print("Temp:");
-    Serial.print(bmp180.getTempInCDirect());
-    Serial.println(" \'C");
-    Serial.println("Prssure:");
-    Serial.print(bmp180.getPressureDirect());
-    Serial.println(" Pa");
-    delay(1000);
+void loop() {
+  Serial.print("Temp:");
+  Serial.print(bmp180.getTempInCDirect());
+  Serial.println(" \'C");
+  Serial.println("Prssure:");
+  // you can use this function to get pressure data directly without measure
+  // temp.
+  Serial.print(bmp180.getPressureDirect());
+  Serial.println(" Pa");
+  delay(1000);
 }
